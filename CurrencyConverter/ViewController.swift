d827289acfcaba7b087202d401fb20914cf58f44
Eray İnal <114001362+erayinal@users.noreply.github.com>
2024-02-16 23:53:10 +0300

    Currency Converter

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Eray İnal on 13.02.2024.
+//
+
+//1 API nedir? Açılımı 'Application Programming Interface'. API ile bir sunucudan herhangi bir programa herhangi bir mobil uygulamaya veri aktarımı yapabiliyoruz
+
+//.1 JSON nedir? Açılımı 'JavaScript Object Notation'. Bu bir veri alışverişi yapma formatıdır(Karmaşık verileri yapısal bir şekilde göstermeye yarayan bir gösterim formatı), biz API ile verileri alırken JSON formatında alırız ve kullanırız
+
+//2 Şimdi öncelikle google'a 'Currency Conversion API' yazarak 'https://fixer.io' bu siteye gidelim. API'da iki tane önemli şey var, birincisi istek yollamak ikincisi cevap almak üçüncüsü ise aldığımız cevabı işlemek 
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
