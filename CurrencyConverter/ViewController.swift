41e8cc678e583f2b23c1bda8ffb06565a0493dc5
Eray İnal <114001362+erayinal@users.noreply.github.com>
2024-02-20 22:51:46 +0300

    Project completion update

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -53,12 +53,45 @@ class ViewController: UIViewController {
                 let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil) //....3 Alert'e bir OK button'u ekleyelim
                 alert.addAction(okButton)
                 self.present(alert, animated: true, completion: nil)
-                
-                //.....3 Böylece birinci adımı yapmış olduk yani siteye gitmeyi başardık. Şimdi 'else' içerisinde ikinci adıma geçelim:
+                //.....3 buraya kadar geldikten sonra 'task' ın dışına çıkarak 'task.resume()' yazmamız gerekiyor
+                //......3 Böylece birinci adımı yapmış olduk yani siteye gitmeyi başardık. Şimdi 'else' içerisinde ikinci adıma geçelim:
             }
             else{
-                //4 
+                //4 ikinic adıma başlamadan önce, veriyi alacağımız site 'http' kullanoıyorsa swift buna izin vermiyor, bu yüzden 'Info' sayfasından buna izin vermeliyiz: Info listesinden '+' ya basıktan sonra 'App Transport Security Settings' seçiyoruz, yanındaki ok aşağıyı gösterecek şekilde tekrar '+'ya basarak 'Allow Arbitrary Loads' seçeneğini 'YES'e çevirince izin vermiş oluyoruz
                 if(data != nil){
+                    do{
+                        let jsonResponse = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String, Any>
+                        
+                        DispatchQueue.main.async {
+                            //.4 Buraya kadar da ikinci adım bitmiş oluyor
+                            //5 Şimdi en son adım olan üçüncü adıma geçelim: Öncesinde hemen bu jsonResponse'un sonuna 'as! Dictionary<String, Any>' yazarak cast edelim ve hemen istediğimiz elemanı alabiliyor muyuz test edelim:
+                            //print(jsonResponse["success"]) //.5 Denedikten sonra yorum satırına alalım
+                            if let rates = jsonResponse["rates"] as? [String : Any]{ //6 Burada aslında [String: String] olarak cast etmemize gerek yok aslında çünkü biliyoruz rates içerisindeki verilerin String:Double olduğunu ama bilelim diye yazdım
+                                
+                                if let cad = rates["CAD"] as? Double{
+                                    self.cadLabel.text = "CAD: " + String(cad)
+                                }
+                                if let chf = rates["CHF"] as? Double{
+                                    self.chfLabel.text = "CHF: " + String(chf)
+                                }
+                                if let gbp = rates["GBP"] as? Double{
+                                    self.gbpLabel.text = "GBP: " + String(gbp)
+                                }
+                                if let jpy = rates["JPY"] as? Double{
+                                    self.jpyLabel.text = "JPY: " + String(jpy)
+                                }
+                                if let usd = rates["USD"] as? Double{
+                                    self.usdLabel.text = "USD: " + String(usd)
+                                }
+                                if let turkish = rates["TRY"] as? Double{
+                                    self.tryLabel.text = "TRY: " + String(turkish)
+                                }
+                                
+                            }
+                        }
+                    } catch{
+                        print("Error...")
+                    }
                     
                 }
                 
@@ -66,6 +99,7 @@ class ViewController: UIViewController {
             
             
         }
+        task.resume() //.....3
         
     }
     
